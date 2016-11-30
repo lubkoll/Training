52b8e19a7a8ab9abcbd4cebe582d9e35016f79a6
Lars Lubkoll <lars.lubkoll@posteo.de>
2016-11-30 20:31:54 +0100

    Add example for initializer lists

---
+++ initializer_list/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <type_traits>
+#include <vector>
+
+
+int main(int argc, char *argv[])
+{
+    std::cout << std::boolalpha;
+
+    auto list = { 'm', 'e', 'e' };
+
+    auto is_initializer_list =
+            std::is_same<
+                std::initializer_list<char>,
+                decltype(list)
+            >::value;
+
+    std::cout << "is initializer list: " << is_initializer_list << std::endl;
+
+    // Main use-case: Initialize containers
+    std::vector<int> v = { 0, 1, 1, 2, 3, 5 };
+    std::vector<char> w = list;
+
+    // This does not compile
+    // auto value_list = { 1, 2.0, 3u };
+}
