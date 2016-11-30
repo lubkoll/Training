9e3023d3e6360536f7cd9136a0d072741d8635d0
Lars Lubkoll <lars.lubkoll@posteo.de>
2016-11-30 22:02:29 +0100

    Add example for <chrono>

---
+++ chrono/main.cpp
@@ -0,0 +1,30 @@
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+using namespace std::chrono;
+
+int main()
+{
+    vector<string> v;
+    string start_of_lorem_ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pellentesque consequat purus ut fringilla. Morbi pharetra odio purus, eu viverra leo accumsan non. Vivamus nec metus nec nibh faucibus commodo. Aliquam quis ipsum egestas ante facilisis rhoncus ut nec nisi. Nulla quis tortor imperdiet, varius nibh nec, egestas nulla. Nullam porttitor interdum lacinia. Sed cursus eros a tortor malesuada, id commodo nunc condimentum. Suspendisse vel lacinia erat. Nunc malesuada sapien eu erat venenatis, ut laoreet justo congue. Suspendisse eu lacus ante. Cras vel tempor ligula.";
+
+
+    auto start_time = system_clock::now();
+
+    for(auto i=0; i<1'000'000; ++i)
+        v.push_back(start_of_lorem_ipsum);
+
+    time_point<system_clock> end_time = system_clock::now();
+    auto elapsed = duration_cast<milliseconds>(end_time - start_time);
+    cout << "elapsed time: " << elapsed.count() << " ms\n\n";
+
+    cout << "milliseconds since 1.1.1970: " << end_time.time_since_epoch().count() << endl;
+
+    // to/from_time_t only exists for std::system_clock, not for std::steady_clock or std::high_resolution_clock
+    auto time = system_clock::to_time_t(end_time);
+    cout << "time: " << put_time(localtime(&time), "%c %Z") << endl;
+}
