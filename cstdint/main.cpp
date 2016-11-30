6146a663908cbd446daea71a9e2fd0f15663d00f
Lars Lubkoll <lars.lubkoll@posteo.de>
2016-11-30 20:55:59 +0100

    Add example for <cstdint>

---
+++ cstdint/main.cpp
@@ -0,0 +1,18 @@
+#include <cstdint>
+#include <iostream>
+
+#define PRINT(T) \
+    std::cout << "size of "#T": " << sizeof(T) << std::endl;
+
+
+int main(int argc, char *argv[])
+{
+    PRINT(std::int8_t);
+    PRINT(std::int16_t);
+    PRINT(std::int32_t);
+    PRINT(std::int64_t);
+    std::cout << std::endl;
+    PRINT(std::intmax_t);
+    PRINT(std::intptr_t);
+    PRINT(std::int_fast8_t);
+}
