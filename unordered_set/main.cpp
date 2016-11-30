d5b0f8b6b6da1bc760fc9be86b2462d30ae08625
Lars Lubkoll <lars.lubkoll@posteo.de>
2016-11-30 20:16:53 +0100

    Add example for unordered_set

--- unordered_set/main.cpp
+++ unordered_set/main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <unordered_set>
 
-using namespace std;
+struct Point {
+    int x, y;
+};
 
-int main(int argc, char *argv[])
+bool operator==(const Point& x, const Point& y)
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    return x.x == y.x && x.y == y.y;
+}
+
+namespace std
+{
+    template<> struct hash<Point>
+    {
+        typedef Point argument_type;
+        typedef std::size_t result_type;
+        result_type operator()(argument_type const& s) const
+        {
+            return std::hash<int>()(s.x) ^ std::hash<int>()(s.y);
+        }
+    };
+}
+
+int main() {
+    // contains unique elements
+    std::unordered_set<Point> points = {
+        {1, 42},
+        {1, 73},
+        {1, 42}
+    };
+
+    for (const auto& point : points) {
+        std::cout << "(" << point.x << ", " << point.y << ")" << std::endl;
+    }
 }
