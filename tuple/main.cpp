24b929dbdc84a8d79c6e9e9ab950ae1aba4aac51
Lars Lubkoll <lars.lubkoll@posteo.de>
2016-12-02 21:40:27 +0100

    Remove TuplePrinter from main.cpp

--- tuple/main.cpp
+++ tuple/main.cpp
@@ -2,24 +2,7 @@
 #include <string>
 #include <set>
 #include <tuple>
-
-
-template<class Tuple, std::size_t N>
-struct TuplePrinter {
-    static void print(const Tuple& t)
-    {
-        TuplePrinter<Tuple, N-1>::print(t);
-        std::cout << ", " << std::get<N-1>(t);
-    }
-};
-
-template<class Tuple>
-struct TuplePrinter<Tuple, 1> {
-    static void print(const Tuple& t)
-    {
-        std::cout << std::get<0>(t);
-    }
-};
+#include "tuple_printer.hh"
 
 
 template<class... Args>
