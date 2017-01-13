e4ac7cf8d6e9f613ec48518d83489a23f16f07ef
Lars Lubkoll <lars.lubkoll@posteo.de>
2017-01-13 22:36:14 +0100

    Finish interface of std::array

--- array/main.cpp
+++ array/main.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <array>
 #include <iostream>
+#include <type_traits>
 
 // In C++11, constexpr implies const,
 // in C++14 this is not the case any more.
@@ -8,6 +9,8 @@ const constexpr int dim = 3;
 
 int main()
 {
+    std::cout << std::boolalpha;
+
     // Initialize array using std::initializer_list<int>
     std::array<int, dim> v = { 1, 2, 3 };
 
@@ -18,5 +21,47 @@ int main()
     // Print with range-based for-loop
     for(auto entry : v)
         std::cout << entry << " ";
+    std::cout << "\n";
+
+    // Access underlying type:
+    std::cout << "is int: " << std::is_same< int, decltype(v)::value_type >::value << std::endl;
+    std::cout << "is first entry of type int: " << std::is_same< int, std::tuple_element<0, decltype(v)>::type >::value << std::endl;
+
+    // Public member functions:
+    // Access via square-bracket-operator
+    std::cout << "v[2] = " << v[2] << std::endl;
+
+    // Access via, possibly throwing, at()
+    int v3 = 0;
+    try {
+        int tmp = v.at(3);
+        v3 = tmp;
+    } catch (const std::out_of_range& exception) {
+        std::cout << "v.at(3) throws" << std::endl;
+    }
+    std::cout << "v3 = " << v3 << std::endl;
+
+    std::cout << "first and last element: " << v.front() << ", " << v.back() << std::endl;
+
+    // Direct access to underlying data
+    std::for_each(v.data(), v.data()+3,
+                  [](int i) { std::cout << i << " "; });
+
+    // const reverse iterator -> see also begin()/end(), cbegin()/cend() and rbegin()/rend()
+    std::for_each(v.crbegin(), v.crend(),
+                  [](int i) { std::cout << i << " "; });
+    std::cout << "\n";
+
+    std::cout << "empty: " << v.empty() << std::endl;
+    std::cout << "size: " << v.size() << " or " << std::tuple_size<decltype(v)>::value << std::endl;
+    std::cout << "max_size: " << v.max_size() << std::endl;
+
+    // Non-member functions
+    auto w = v;
+    w.fill(1);
+    swap(v,w);
+
+    std::cout << "v < w: " << (v < w) << std::endl;
+    // and all the other comparison operators...
 }
 
